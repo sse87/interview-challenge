c101a24adc3d1def38f98687fe2a63554c83617d
David Bachmann <dabbi@dabbi.is>
2021-01-05 15:02:47 +0000

    Start of theme

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  breakpoints: {
+    sm: 375,
+    md: 768,
+    lg: 1024,
+    xl: 1440,
+  },
+  colors: {
+    dark: "#333",
+    light: "#FFF",
+    primary: "#FEC483",
+    text: "#1A1A1F",
+  },
+};
