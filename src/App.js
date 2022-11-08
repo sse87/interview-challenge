22739ff3652a577495aa95cb31fba91c86b63836
davidbachmann <dabbi@dabbi.is>
2022-11-08 09:43:55 +0000

    Update project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import data from "./data.json";
+import { theme } from "./theme";
 
 function App() {
+  /* Provided data */
+  console.log(data);
+  /* Provided theme (if needed) */
+  console.log(theme);
+
   return (
     <div className="App">
       {/* Example component */}
       <Navigation />
-      {/* Example svg import */}
+
       <img src={logo} alt="Werk logo" />
 
-      <h1>Hello world</h1>
+      <h1>
+        Hello world. The font <em>Work Sans</em> is provided.
+      </h1>
     </div>
   );
 }
